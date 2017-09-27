5fd452f88171a43cb0831017aa81118996bdb5de
CYB3RL1F3 <cyberlife.music@gmail.com>
2017-09-27 23:01:22 +0200

    upgrade webpack to v3.6.0

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ![](http://jpsierens.com/wp-content/uploads/2016/06/react-eco-wp.gif)
 
-# webpack-react-redux
+# react-redux-boilerplate
 A boilerplate for playing around with react, redux and react-router with the help of webpack.
 
 Contains:
--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ var path = require('path');
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var DashboardPlugin = require('webpack-dashboard/plugin');
+var helpers = require('./helpers');
 
 module.exports = {
     devtool: 'eval-source-map',
@@ -26,42 +27,67 @@ module.exports = {
           filename: 'index.html'
         }),
         new DashboardPlugin(),
-        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.optimize.OccurrenceOrderPlugin(),
         new webpack.HotModuleReplacementPlugin(),
         new webpack.NoErrorsPlugin(),
         new webpack.DefinePlugin({
           'process.env.NODE_ENV': JSON.stringify('development')
         })
     ],
-    eslint: {
-        configFile: '.eslintrc',
-        failOnWarning: false,
-        failOnError: false
-    },
     module: {
-        preLoaders: [
+        rules: [
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                loader: 'eslint'
-            }
+                use: [
+                    "babel-loader",
+                    "eslint-loader",
+                ],
+            },
+            {
+              test: /\.json$/,
+              use: 'json-loader'
+            },
+
+            /**
+             * to string and sass loader support for *.scss files (from Angular components)
+             * Returns compiled css content as string
+             *
+             */
+            {
+              test: /\.scss$/,
+              use: ['to-string-loader', 'css-loader', 'sass-loader'],
+              exclude: [helpers.root('src', 'styles')]
+            },
+            {
+              test: /\.html$/,
+              use: 'raw-loader',
+              exclude: [helpers.root('src/index.html')]
+            },
+
+            /* File loader for supporting images, for example, in CSS files.
+             */
+            {
+              test: /\.(jpg|png|gif|ttf|woff|woff2|otf|svg)$/,
+              use: 'file-loader'
+            },
         ],
         loaders: [
             {
                 test: /\.js?$/,
                 exclude: /node_modules/,
-                loader: 'babel'
+                loader: 'babel-loader'
             },
             {
                 test: /\.json?$/,
-                loader: 'json'
+                loader: 'json-loader'
             },
             {
                 test: /\.scss$/,
-                loader: 'style!css?modules&localIdentName=[name]---[local]---[hash:base64:5]!sass'
+                loader: 'style-loader!css-loader?modules&localIdentName=[name]---[local]---[hash:base64:5]!sass'
             },
-            { test: /\.woff(2)?(\?[a-z0-9#=&.]+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff' },
-            { test: /\.(ttf|eot|svg)(\?[a-z0-9#=&.]+)?$/, loader: 'file' }
+            { test: /\.woff(2)?(\?[a-z0-9#=&.]+)?$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff' },
+            { test: /\.(ttf|eot|svg)(\?[a-z0-9#=&.]+)?$/, loader: 'file-loader' }
         ]
     }
 };
