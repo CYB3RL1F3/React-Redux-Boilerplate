d6c62b87033f77f3446564212a8ac0d42f8c083a
CYB3RL1F3 <cyberlife.music@gmail.com>
2017-09-28 01:16:34 +0200

    aliases are now dynamically generated by parsing app folder

--- aliases.js
+++ aliases.js
@@ -1,19 +1,19 @@
 const path = require('path');
+const fs = require('fs');
 
-const aliases = [
-    'actions',
-    'components',
-    'containers',
-    'reducers',
-    'store',
-    'styles',
-    'routes'
-].reduce((acc, dir) => {
+const dirs = [];
+fs.readdirSync(`${__dirname}/app`).forEach(file => {
+    if (!(/(\.)/.test(file)) || (/(\.js)/.test(file) && file !== 'index.js')) {
+        dirs.push(file.replace('.js', ''));
+    }
+});
+
+const aliases = dirs.reduce((acc, dir) => {
     acc[dir] = path.resolve('app', dir);
-    console.log(`define alias for ${dir} => ${acc[dir]}`);
     return acc;
 }, {});
 
+aliases.assets = path.join(__dirname, 'assets');
 aliases.package_json = path.join(__dirname, 'package.json');
 
 module.exports = aliases;
